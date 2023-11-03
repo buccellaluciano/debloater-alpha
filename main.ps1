cf408f55b3c6df85ca13920eaf9f3df20a9c9be0
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-03 18:29:50 -0300

    a
    a

--- main.ps1
+++ main.ps1
@@ -6,14 +6,22 @@ $main_form.Height = 400
 $main_form.AutoSize = $true
 
 $button = New-Object System.Windows.Forms.Button
-$Button.Text = "Coxis"
+$Button.Text = "Desactivar Boton de busqueda"
 $button.Location = New-Object System.Drawing.Point(400, 400)
-
-
-$Button.Add_Click({C:\Users\miste\OneDrive\Escritorio\Desktop\github\debloater-alpha\disable-copilot.reg})
-
+$Button.Add_Click({./regfiles\disable-searchicon.reg})
 $main_form.Controls.Add($button)
 
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "Activar Boton de busqueda"
+$button.Location = New-Object System.Drawing.Point(300, 300)
+$Button.Add_Click({./regfiles\enable-searchicon.reg})
+$main_form.Controls.Add($button)
+
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "Activar Barra"
+$button.Location = New-Object System.Drawing.Point(200, 200)
+$Button.Add_Click({./regfiles\enable-searchbox.reg})
+$main_form.Controls.Add($button)
 
 
 
