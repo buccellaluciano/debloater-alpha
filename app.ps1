646b35b3ecf9a934f3bc00b5415092e2d7d8c9b9
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-08 14:27:42 -0300

    update
    paso los scripts principales a otra ventana, la main va a ser el inciio e introduccion

---
+++ app.ps1
@@ -0,0 +1,41 @@
+Add-Type -assembly System.Windows.Forms
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text ='GUI for my PoSh script'
+$main_form.Width = 800
+$main_form.Height = 400
+$main_form.AutoSize = $true
+
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "Desactivar Boton de busqueda"
+$button.Location = New-Object System.Drawing.Point(400, 400)
+$Button.Add_Click({./regfiles\disable-searchicon.reg})
+$main_form.Controls.Add($button)
+
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "Activar Boton de busqueda"
+$button.Location = New-Object System.Drawing.Point(300, 300)
+$Button.Add_Click({./regfiles\enable-searchicon.reg})
+$main_form.Controls.Add($button)
+
+
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "Activar Barra"
+$button.Location = New-Object System.Drawing.Point(200, 200)
+$Button.Add_Click({./regfiles\enable-searchbox.reg})
+$main_form.Controls.Add($button)
+
+
+$alpath = "C:\Users\estudiante\Documents\GitHub\debloater-alpha\applist.txt"
+$Applist = Get-Content -Path $alpath
+
+
+
+$i = 1 
+foreach ($Line in $Applist) {
+    Write-Host "$Line $i :";
+    $i++
+}
+
+
+
+$main_form.ShowDialog()
--- main.ps1
+++ main.ps1
@@ -6,23 +6,9 @@ $main_form.Height = 400
 $main_form.AutoSize = $true
 
 $button = New-Object System.Windows.Forms.Button
-$Button.Text = "Desactivar Boton de busqueda"
+$Button.Text = "Inciar Debloater"
 $button.Location = New-Object System.Drawing.Point(400, 400)
-$Button.Add_Click({./regfiles\disable-searchicon.reg})
+$Button.Add_Click({./app.ps1})
 $main_form.Controls.Add($button)
-
-$button = New-Object System.Windows.Forms.Button
-$Button.Text = "Activar Boton de busqueda"
-$button.Location = New-Object System.Drawing.Point(300, 300)
-$Button.Add_Click({./regfiles\enable-searchicon.reg})
-$main_form.Controls.Add($button)
-
-$button = New-Object System.Windows.Forms.Button
-$Button.Text = "Activar Barra"
-$button.Location = New-Object System.Drawing.Point(200, 200)
-$Button.Add_Click({./regfiles\enable-searchbox.reg})
-$main_form.Controls.Add($button)
-
-
 
 $main_form.ShowDialog()
