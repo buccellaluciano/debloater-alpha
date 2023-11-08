67cbdad6d0a19d859f7f7c8b41257cd933600b4b
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-08 16:30:10 -0300

    nashe

--- main.ps1
+++ main.ps1
@@ -8,7 +8,12 @@ $main_form.AutoSize = $true
 $button = New-Object System.Windows.Forms.Button
 $Button.Text = "Inciar Debloater"
 $button.Location = New-Object System.Drawing.Point(400, 400)
-$Button.Add_Click({./app.ps1})
+$Button.Add_Click({
+    $location = Get-ChildItem main.ps1
+    Write-Host $location
+    
+
+})
 $main_form.Controls.Add($button)
 $PSVersionTable
 $main_form.ShowDialog()
