d294b46fcebd3c0cda274cfbf15ed1ca05337aa2
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-08 16:10:40 -0300

    agrega el icono y lo muestra

--- main.ps1
+++ main.ps1
@@ -5,10 +5,18 @@ $main_form.Width = 800
 $main_form.Height = 400
 $main_form.AutoSize = $true
 
+$iconPath = ".\img\debloaterIcon.ico"
+$icon = [System.Drawing.Icon]::ExtractAssociatedIcon($iconPath)
+$main_form.Icon = $icon
+
 $button = New-Object System.Windows.Forms.Button
 $Button.Text = "Inciar Debloater"
 $button.Location = New-Object System.Drawing.Point(400, 400)
 $Button.Add_Click({./app.ps1})
 $main_form.Controls.Add($button)
 $PSVersionTable
+
+
+
 $main_form.ShowDialog()
+
