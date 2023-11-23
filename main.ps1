23bf79f58435e518098b2eacb2a6f14d75858bc4
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-23 03:59:35 -0300

    openscript
    funcion para conseguir el path de cualquier y abrirlo creada, se llama openscript

--- main.ps1
+++ main.ps1
@@ -9,31 +9,52 @@ $iconPath = ".\img\debloaterIcon.ico"
 $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($iconPath)
 $main_form.Icon = $icon
 
+$pwpath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+
+$adminCheck = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+if (-not $adminCheck) {
+    # Si no se están ejecutando con privilegios de administrador, relanza el script con privilegios elevados
+
+    # Crea un objeto ProcessStartInfo para configurar la ejecución con privilegios elevados
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powershell"
+    $startInfo.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    $startInfo.Verb = "runas"  # Esto solicitará permisos elevados
+
+    # Crea un objeto Process y lo inicia
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start()
+
+    # Sale del script actual
+    Exit
+}
+$mainpath= $MyInvocation.MyCommand.Definition
+
+# abre el script indicado, consigue la direccion del mismo a partir de la variable script, la cual obtiene un valor a partir de apretar un boton.
+function openscript {
+    $defpath = $mainpath.Replace("main.ps1", "$script")
+    Start-Process -FilePath $pwpath -ArgumentList $defpath
+}
+
+
 $button = New-Object System.Windows.Forms.Button
-$Button.Text = "Inciar Debloater"
-$button.Location = New-Object System.Drawing.Point(400, 400)
-$locatio = Get-ChildItem debloater.ps1
-$Button.Add_Click({Start-Process "$locatio"})
-$main_form.Controls.Add($button)
-$PSVersionTable
-
-$RemoveKeys                      = New-Object system.Windows.Forms.Button
-$RemoveKeys.text                 = "Remove Keys"
-$RemoveKeys.width                = 150
-$RemoveKeys.height               = 50
-$RemoveKeys.location             = New-Object System.Drawing.Point(20,425)
-$RemoveKeys.Font                 = New-Object System.Drawing.Font('Microsoft Sans Serif',12,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-$RemoveKeys.ForeColor            = [System.Drawing.ColorTranslator]::FromHtml("#ffffff")
-$RemoveKeys.BackColor            = [System.Drawing.ColorTranslator]::FromHtml("#4a90e2")
-$main_form.Controls.Add($RemoveKeys)
-
-
-$button = New-Object System.Windows.Forms.Button
-$Button.Text = "Inciar Debloater"
+$Button.Text = "asheeee"
 $button.Location = New-Object System.Drawing.Point(100, 200)
-$locatio = Get-ChildItem debloater.ps1
-$Button.Add_Click({C:\Users\estudiante\Documents\GitHub\debloater-alpha\regfiles\change-class.reg})
+$Button.Add_Click({
+        $script = "debloater.ps1"
+        openscript
+})
+$main_form.Controls.Add($button)
+
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "nachopolis"
+$button.Location = New-Object System.Drawing.Point(400, 300)
+$Button.Add_Click({
+        $script = "create-folder.ps1"
+        openscript
+})
 $main_form.Controls.Add($button)
 
 $main_form.ShowDialog()
-
