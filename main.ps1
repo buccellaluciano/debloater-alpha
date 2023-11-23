4f1a8c2b22c964a9a322e6ffaf244bec27aa8d83
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-23 15:30:26 -0300

    GOD CHANGE

--- main.ps1
+++ main.ps1
@@ -1,34 +1,58 @@
-Add-Type -assembly System.Windows.Forms
-$main_form = New-Object System.Windows.Forms.Form
-$main_form.Text ='Debloater'
-$main_form.Width = 800
-$main_form.Height = 400 
-$main_form.AutoSize = $true
+Add-Type -AssemblyName System.Windows.Forms
+
+$formPrincipal = New-Object System.Windows.Forms.Form
+$formPrincipal.Text = 'Super Debloater!'
+$formPrincipal.Size = New-Object System.Drawing.Size(800,600)
+$formPrincipal.StartPosition = 'CenterScreen'
 
 $iconPath = ".\img\debloaterIcon.ico"
 $icon = [System.Drawing.Icon]::ExtractAssociatedIcon($iconPath)
-$main_form.Icon = $icon
+$formPrincipal.Icon = $icon
 
-$button = New-Object System.Windows.Forms.Button
-$Button.Text = "Inciar Debloater"
-$button.Location = New-Object System.Drawing.Point(400, 400)
-$locatio = Get-ChildItem debloater.ps1
-$Button.Add_Click({Start-Process "$locatio"})
-$main_form.Controls.Add($button)
-$PSVersionTable
+$formPrincipal.BackColor = [System.Drawing.Color]::LightGray
 
 
-$button = New-Object System.Windows.Forms.Button
-$Button.Text = "Inciar Debloater"
-$button.Location = New-Object System.Drawing.Point(100, 200)
-$Button.Add_Click({$buttons})
-$main_form.Controls.Add($button)
+Import-Module -DisableNameChecking .\src\lib\UI\UI-Creator.psm1 -Force
 
+$tabControl = Add-TabControl -Form $formPrincipal -X 0 -Y 0 -Width 800 -Height 600
 
-$buttons=[windows.forms.messagebox]::show('body','title','YesNo')
-[System.Security.SecurityException]::show
+$tabPage1 = Add-TabPage -TabControl $tabControl -Text "Tweaks"
+$tabPage2 = Add-TabPage -TabControl $tabControl -Text "Install"
+$tabPage3 = Add-TabPage -TabControl $tabControl -Text "DEBUG"
+
+############TAB 1############
 
 
 
-$main_form.ShowDialog()
+$butUninstalWindows = Add-Button -Control $tabPage1 -Text "Developer test" -X 10 -Y 10 -Width 100 -Height 20
+############TAB 1############
+
+
+
+
+
+
+
+
+############TAB 2############
+$button2 = Add-Button -Control $tabPage2 -Text "Button 2" -X 10 -Y 10 -Width 100 -Height 20
+############TAB 3############
+
+
+
+
+
+############TAB 3############
+$button3 = Add-Button -Control $tabPage3 -Text "Button 2" -X 10 -Y 10 -Width 100 -Height 20
+############TAB 3############
+
+#$buttons=[windows.forms.messagebox]::show('body','title','YesNo')
+#[System.Security.SecurityException]::show
+
+
+$butUninstalWindows.Add_Click( {
+    Write-Host "DevTest"
+})
+
+$formPrincipal.ShowDialog()
 
