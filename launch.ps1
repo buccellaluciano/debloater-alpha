bd19e0ec31832eab3b65d41814d302ece4d3e81c
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-24 05:31:03 -0300

    el mismo mensaje de antes

--- launch.ps1
+++ launch.ps1
@@ -39,10 +39,180 @@ $button = New-Object System.Windows.Forms.Button
 $Button.Text = "nachopolis"
 $button.Location = New-Object System.Drawing.Point(400, 300)
 $Button.Add_Click({
-        $script = "debloater.ps1"
+        $oscript = "debloater.ps1"
         openscript
 })
 $formPrincipal.Controls.Add($button)
 
+function uninstallpack {
+    foreach ($i in $apps) {
+        
+        if ($selapp -eq $i){
+            $selapp = $i
+            Write-Host ("$selapp")
+            #la linea de abajo hace que lo de arriba desinstale el programa que elijas, no lo activen, a no ser...
+            # Get-AppxPackage -Name $selapp -AllUsers | Remove-AppxPackage
+        }
+        
+    }
+}
 
-$formPrincipal.ShowDialog()
+$button = New-Object System.Windows.Forms.Button
+$Button.Text = "JUAN"
+$button.Location = New-Object System.Drawing.Point(350, 250)
+$Button.Add_Click({
+    $selapp = "Microsoft.BingFinance"
+        uninstall
+})
+$formPrincipal.Controls.Add($button)
+
+
+
+function Changeregs {
+    
+    $regfilesloc = Get-ChildItem regfiles
+    $regfiles = Get-Content -Path $regfilesloc
+    
+    $j
+    foreach ($Line in $regfiles){
+        Write-Host "$Line $j :";
+        $j++
+    }
+    
+    Param($Enabled)
+    Try{
+        if ($Enabled -eq $false){
+            Write-Host "Habilitando..."
+            $value = 1
+        }
+        else {
+            Write-Host "Desabilitando..."
+            $value = 0
+        }
+        $Path = "$regpath"
+        Set-ItemProperty -Path $Path -Name $regname -Value $value
+    
+    }
+    Catch [System.Security.SecurityException] {
+        Write-Warning "Unable to set $Path\$Name to $Value due to a Security Exception"
+    }
+}
+
+    Set-ItemProperty -Path $Path -Name BingSearchEnabled -Value $value
+
+
+    $apps = @("Clipchamp.Clipchamp"
+    "Microsoft.3DBuilder"
+    "Microsoft.549981C3F5F10"   #Cortana app
+    "Microsoft.BingFinance"
+    "Microsoft.BingFoodAndDrink"            
+    "Microsoft.BingHealthAndFitness"         
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingTranslator"
+    "Microsoft.BingTravel" 
+    "Microsoft.BingWeather"
+    "Microsoft.Getstarted"   # Cannot be uninstalled in Windows 11
+    "Microsoft.Messaging"
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftPowerBIForWindows"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.MixedReality.Portal"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.News"
+    "Microsoft.Office.OneNote"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.Print3D"
+    "Microsoft.SkypeApp"
+    "Microsoft.Todos"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.XboxApp"   # Old Xbox Console Companion App, no longer supported
+    "Microsoft.ZuneVideo"
+    "MicrosoftTeams"   # Only removes the personal version (MS Store), does not remove business/enterprise version of teams
+    "ACGMediaPlayer"
+    "ActiproSoftwareLLC"
+    "AdobeSystemsIncorporated.AdobePhotoshopExpress"
+    "Amazon.com.Amazon"
+    "AmazonVideo.PrimeVideo"
+    "Asphalt8Airborne" 
+    "AutodeskSketchBook"
+    "CaesarsSlotsFreeCasino"
+    "COOKINGFEVER"
+    "CyberLinkMediaSuiteEssentials"
+    "DisneyMagicKingdoms"
+    "Disney"
+    "Dolby"
+    "DrawboardPDF"
+    "Duolingo-LearnLanguagesforFree"
+    "EclipseManager"
+    "Facebook"
+    "FarmVille2CountryEscape"
+    "fitbit"
+    "Flipboard"
+    "HiddenCity"
+    "HULULLC.HULUPLUS"
+    "iHeartRadio"
+    "Instagram"
+    "king.com.BubbleWitch3Saga"
+    "king.com.CandyCrushSaga"
+    "king.com.CandyCrushSodaSaga"
+    "LinkedInforWindows"
+    "MarchofEmpires"
+    "Netflix"
+    "NYTCrossword"
+    "OneCalendar"
+    "PandoraMediaInc"
+    "PhototasticCollage"
+    "PicsArt-PhotoStudio"
+    "Plex"
+    "PolarrPhotoEditorAcademicEdition"
+    "Royal Revolt"
+    "Shazam"
+    "Sidia.LiveWallpaper"
+    "SlingTV"
+    "Speed Test"
+    "Spotify"
+    "TikTok"
+    "TuneInRadio"
+    "Twitter"
+    "Viber"
+    "WinZipUniversal"
+    "Wunderlist"
+    "XING"
+    "Microsoft.GetHelp"                      # Required for some Windows 11 Troubleshooters
+    "Microsoft.MSPaint"                      # Paint 3D
+    "Microsoft.OutlookForWindows"            # New mail app
+    "Microsoft.Paint"                        # Classic Paint
+    "Microsoft.People"                       # Required for & included with Mail & Calendar
+    "Microsoft.PowerAutomateDesktop"
+    "Microsoft.RemoteDesktop"
+    "Microsoft.ScreenSketch"                 # Snipping Tool
+    "Microsoft.Whiteboard"                   # Only preinstalled on devices with touchscreen and/or pen support
+    "Microsoft.Windows.Photos"
+    "Microsoft.WindowsCalculator"
+    "Microsoft.WindowsCamera"
+    "Microsoft.windowscommunicationsapps"    # Mail & Calendar
+    "Microsoft.WindowsStore"                 # Microsoft Store, WARNING: This app cannot be reinstalled!
+    "Microsoft.WindowsTerminal"              # New default terminal app in windows 11
+    "Microsoft.Xbox.TCUI"                    # UI framework, seems to be required for MS store, photos and certain games
+    "Microsoft.XboxIdentityProvider"         # Xbox sign-in framework, required for some games
+    "Microsoft.XboxSpeechToTextOverlay"      # Might be required for some games, WARNING: This app cannot be reinstalled!
+    "Microsoft.YourPhone"                    # Phone link
+    "Microsoft.ZuneMusic"                    # Modern Media Player
+    )
+
+    $gamingapps =@(
+    # The apps below will NOT be uninstalled unless selected during the custom setup selection or when
+    #  launching the script with the '-RemoveGamingApps' parameter. 
+    "Microsoft.GamingApp"                    # Modern Xbox Gaming App, required for installing some PC games
+    "Microsoft.XboxGameOverlay"              # Game overlay, required/useful for some games
+    "Microsoft.XboxGamingOverlay"            # Game overlay, required/useful for some games)
+    )
+    
+    $formPrincipal.ShowDialog()
