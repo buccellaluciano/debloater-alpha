67917280d65ef2f7cd653503236ed9689e366784
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-24 22:35:33 -0300

    agregado el open ps1

---
+++ open.ps1
@@ -0,0 +1,21 @@
+
+$adminCheck = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+if (-not $adminCheck) {
+    # Si no se están ejecutando con privilegios de administrador, relanza el script con privilegios elevados
+
+    # Crea un objeto ProcessStartInfo para configurar la ejecución con privilegios elevados
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powershell"
+    $startInfo.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    $startInfo.Verb = "runas"  # Esto solicitará permisos elevados
+
+    # Crea un objeto Process y lo inicia
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start()
+
+    # Sale del script actual
+    Exit
+}
+. "$PSScriptRoot\main.ps1"
