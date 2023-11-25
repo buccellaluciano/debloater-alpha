e8429eb945aefb3a25833a5d5035f2fc03f7a252
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-24 23:24:46 -0300

    agregando txt con cambios de registro

--- main.ps1
+++ main.ps1
@@ -53,12 +53,6 @@ $tabPage3 = Add-TabPage -TabControl $tabControl -Text "DEBUG"
 #[System.Security.SecurityException]::show
 
 
-
-
-
-
-
-
 ####RODRIGO Y JULIAN: TIENEN QUE HACER FUNCIONES DE LOS BOTONES.###########
 
 
