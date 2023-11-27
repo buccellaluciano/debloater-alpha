7050e095447da325d86c0c016e0d15030ba5b727
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-27 18:55:31 -0300

    lastbtn

--- tabs/Tab3.ps1
+++ tabs/Tab3.ps1
@@ -26,8 +26,8 @@ for ($i = 0; $i -lt $posiciones_control; $i++) {
 }
 
 ############TAB 3############
-$btnTESTa = Add-ImageButton -Control $tabPage3 -ImagePath $global:imagenTroll -X ($formPrincipal.Width / 2 - 400) -Y ($formPrincipal.Width / 2 - 500) -Width 800 -Height 500
-$butTESTa.Add_Click{
+$btnlastbtn = Add-ImageButton -Control $tabPage3 -ImagePath $global:imagenTroll -X ($formPrincipal.Width / 2 - 400) -Y ($formPrincipal.Width / 2 - 500) -Width 800 -Height 500
+$btnlastbtn.Add_Click{
     $global:removebloatware=1
     Confirm-Action -Message "Mira que esto quita todo el bloatware, te vas a quedar sin computadora por unos minutos" -YesAction {upackages}
 }
