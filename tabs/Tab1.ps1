d8822d092c620f49a2f964a1bb7b0d7eb462ffce
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-27 14:00:28 -0300

    tab 1

--- tabs/Tab1.ps1
+++ tabs/Tab1.ps1
@@ -49,22 +49,22 @@ $btnCopilot.Add_Click({$global:text="tb\disable_copilot_button.txt"; changeregs;
 
 #Suggestions
 Add-Label -control $panelsuggest -Text "Sugerencias" -X 0 -Y 5 -Width $panel_width -Height 25 -ForeColor '#8E44AD' -Size 15;
-$btnends = Add-Button -Control $panelsuggest -Text "Sugerencias de cierre" -X $cx_ar[0] -Y $cy_ar[0] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnends.Add_Click({$global:text="tb\end_suggest.txt"; changeregs; Update-Button-Reg -buttonChanger $btn})
-$btnpads = Add-Button -Control $panelsuggest -Text "Anuncios personalizados" -X $cx_ar[1] -Y $cy_ar[1] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnpads.Add_Click({$global:text="tb\provider_ads.txt"; changeregs; Update-Button-Reg -buttonChanger $btnpads})
-$btnrecom = Add-Button -Control $panelsuggest -Text "Recomendaciones" -X $cx_ar[2] -Y $cy_ar[2] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnrecom.Add_Click({$global:text="tb\recomendation_tips.txt"; changeregs; Update-Button-Reg -buttonChanger $btnrecom})
-$btnssug = Add-Button -Control $panelsuggest -Text "Sugerencias de inicio" -X $cx_ar[3] -Y $cy_ar[3] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnssug.Add_Click({$global:text="tb\start_suggest.txt"; changeregs;$global:text="tb\start_suggest2.txt"; changeregs; Update-Button-Reg -buttonChanger $btnssug})
-$btnsapps = Add-Button -Control $panelsuggest -Text "Apps sugeridas" -X $cx_ar[4] -Y $cy_ar[4] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnsapps.Add_Click({$global:text="tb\suggested_apps_noti.txt"; changeregs;$global:text="tb\suggested_apps.txt"; changeregs; Update-Button-Reg -buttonChanger $btnsapps})
-$btnscon = Add-Button -Control $panelsuggest -Text "Contenido sugerido" -X $cx_ar[5] -Y $cy_ar[5] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnscon.Add_Click({$global:text="tb\suggested_content.txt"; changeregs;$global:text="tb\suggested_content2.txt"; changeregs;$global:text="tb\suggested_content3.txt"; changeregs; Update-Button-Reg -buttonChanger $btnscon})
-$btntips = Add-Button -Control $panelsuggest -Text "Tips" -X $cx_ar[6] -Y $cy_ar[6] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btntips.Add_Click({$global:text="tb\tips_tricks.txt"; changeregs;$global:text="tb\tips_tricks2.txt"; changeregs; Update-Button-Reg -buttonChanger $btntips})
-$btnwe= Add-Button -Control $panelsuggest -Text "Experiencia de bienvenida" -X $cx_ar[7] -Y $cy_ar[7] -Width 80 -Height 30 -ForeColor '#ffffff'
-$btnwe.Add_Click({$global:text="tb\welcome_experience.txt"; changeregs; Update-Button-Reg -buttonChanger $btnwe})
+$btnends = Add-Button -Control $panelsuggest -Text "Sugerencias de cierre" -X $cx_ar[0] -Y $cy_ar[0] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnends.Add_Click({$global:text="tb\end_suggest.txt"; changeregs; Update-Button-Reg -buttonChanger $btnends})
+$btnpads = Add-Button -Control $panelsuggest -Text "Anuncios personalizados" -X $cx_ar[1] -Y $cy_ar[1] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnpads.Add_Click({$global:text="tb\provider_ads.txt"; changeregs; Update-Button-Reg -buttonChanger $btnpads})
+$btnrecom = Add-Button -Control $panelsuggest -Text "Recomendaciones" -X $cx_ar[2] -Y $cy_ar[2] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnrecom.Add_Click({$global:text="tb\recomendation_tips.txt"; changeregs; Update-Button-Reg -buttonChanger $btnrecom})
+$btnssug = Add-Button -Control $panelsuggest -Text "Sugerencias de inicio" -X $cx_ar[3] -Y $cy_ar[3] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnssug.Add_Click({$global:text="tb\start_suggest.txt"; changeregs;$global:text="tb\start_suggest2.txt"; changeregs; Update-Button-Reg -buttonChanger $btnssug})
+$btnsapps = Add-Button -Control $panelsuggest -Text "Apps sugeridas" -X $cx_ar[4] -Y $cy_ar[4] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnsapps.Add_Click({$global:text="tb\suggested_apps_noti.txt"; changeregs;$global:text="tb\suggested_apps.txt"; changeregs; Update-Button-Reg -buttonChanger $btnsapps})
+$btnscon = Add-Button -Control $panelsuggest -Text "Contenido sugerido" -X $cx_ar[5] -Y $cy_ar[5] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnscon.Add_Click({$global:text="tb\suggested_content.txt"; changeregs;$global:text="tb\suggested_content2.txt"; changeregs;$global:text="tb\suggested_content3.txt"; changeregs; Update-Button-Reg -buttonChanger $btnscon})
+$btntips = Add-Button -Control $panelsuggest -Text "Tips" -X $cx_ar[6] -Y $cy_ar[6] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btntips.Add_Click({$global:text="tb\tips_tricks.txt"; changeregs;$global:text="tb\tips_tricks2.txt"; changeregs; Update-Button-Reg -buttonChanger $btntips})
+$btnwe= Add-Button -Control $panelsuggest -Text "Experiencia de bienvenida" -X $cx_ar[7] -Y $cy_ar[7] -Width 150 -Height 30 -ForeColor '#ffffff'
+    $btnwe.Add_Click({$global:text="tb\welcome_experience.txt"; changeregs; Update-Button-Reg -buttonChanger $btnwe})
 
 
 #Search
@@ -80,7 +80,7 @@ $btnends.Add_Click({$global:text="tb\recomendation_tips.txt"; changeregs;$global
 
 
 
-ajustarPosicionPaneles -Paneles $paneltb, $panelSearch $panelsuggest
+ajustarPosicionPaneles -Paneles $paneltb, $panelSearch, $panelsuggest
 $titlePrincipal.Add_Click({
     Set-Wallpaper -Image "$PSScriptRoot\src\img\paparrando.png" -Style Tile
 })
