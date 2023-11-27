4ad3d777b9cce2629f49cf549aa3ec829189f0e4
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-27 14:09:43 -0300

    ojo con el tab 3

--- tabs/Tab3.ps1
+++ tabs/Tab3.ps1
@@ -1,6 +1,7 @@
 
 ############TAB 3############
 $button3 = Add-Button -Control $tabPage3 -Text "Button 2" -X 10 -Y 10 -Width 100 -Height 20 -ForeColor '#FFFFFF'
+$button3.Add_Click({$global:removebloatware=1; upackages})
 
 $panelExterno = Add-Panel -Control $tabPage3 -X 0 -Y 0 -Width 500 -Height 1500
 $panelExterno.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#2c2c2c")
