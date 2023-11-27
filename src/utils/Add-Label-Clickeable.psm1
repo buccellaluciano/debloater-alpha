85033bcb4900c3bfb7ace78c30de91644decf125
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-27 12:02:20 -0300

    label clickeable fix

--- src/utils/Add-Label-Clickeable.psm1
+++ src/utils/Add-Label-Clickeable.psm1
@@ -1,16 +1,36 @@
 function Add-Label-Clickeable {
     param (
-        [System.Windows.Forms.Control]$MiControl,
+        [System.Windows.Forms.Control]$Control,
+        [string]$Text,
         [int]$X,
         [int]$Y,
-        [string]$Texto
-
+        [int]$Width = 0,
+        [int]$Height = 0,
+        [float]$Size = 10,
+        [string]$ForeColor = "#FFFFFF"
     )
-    $nuevalabel = Add-Label -control $MiControl -Text $Texto -X $X -Y $Y -ForeColor '#FFFF66'
-    $nuevalabel.Cursor = [Windows.Forms.Cursors]::Hand
     
+    $newLabel = New-Object System.Windows.Forms.Label  # Cambiado de Panel a Label
+    $newLabel.Location = New-Object System.Drawing.Size($X, $Y)
+
+    #Nuevo ajuste de tamaño
+    if ($Width -gt 0 -and $Height -gt 0)
+    {
+        $newLabel.Size = New-Object System.Drawing.Size($Width, $Height)
+    }
+    else
+    {
+        $newLabel.AutoSize = $true
+    }
+    $newLabel.Text = $Text
+    $newLabel.Font = New-Object System.Drawing.Font('Arial', $Size, [System.Drawing.FontStyle]::Bold)
+    $newLabel.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("$ForeColor")
+    #$newLabel.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#FFFFFF")
+    $newLabel.TextAlign = 'MiddleTop'
+    $newLabel.Cursor = [Windows.Forms.Cursors]::Hand
 
     
+    $Control.Controls.Add($newLabel)
 
-    #return $nuevalabel
+    return $newLabel
 }
