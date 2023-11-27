6eb0e968d05db12219c10770c919c4ca60cd8b20
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-26 21:30:06 -0300

    botones cambiados de lugar

--- tabs/Tab2.ps1
+++ tabs/Tab2.ps1
@@ -2,7 +2,7 @@
 ####RODRIGO Y JULIAN: TIENEN QUE HACER FUNCIONES DE LOS BOTONES.###########
 $tituloTab2 = Add-Label -control $tab2Install -Text "Instalacion de software" -X 0 -Y 10 -Width ($formPrincipal.Width-50) -Height 50 -ForeColor '#f54242' -Size 30
 Add-Label -control $tab2Install -Text "DEVTEST" -X 0 -Y 70 -Width ($formPrincipal.Width-50) -Height 30 -ForeColor '#ffffff' -Size 20
-
+$buttonInstalUnin = Add-Button -Control $tab2Install -Text "Button 2" -X 10 -Y 100 -Width 225 -Height 30 -ForeColor '#eeeeee'
 
 ############TAB 2############
 #VARIABLES PARA LOS PANELES DE SANDES, TOCAR SI ES NECESARIO
@@ -39,12 +39,11 @@ for ($i = 0; $i -lt $posiciones_control; $i++) {
 }
 #Elementos de paneles de navegadores
 $labelNav = Add-Label -control $panelNavegadores -Text "Navegadores" -X 0 -Y 5 -Width $panel_width -Height 25 -ForeColor '#8E44AD' -Size 15;
-Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Chrome" -X $cx_ar[0] -Y $cy_ar[0] ; $buttonchrome = Add-Button -Control $panelNavegadores -Text " " -X 120 -Y $cy_ar[0] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonchrome.Add_Click({$global:app="Google.Chrome"; ipackages}) 
-Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Brave" -X $cx_ar[1] -Y $cy_ar[1] ; $buttonbrave = Add-Button -Control $panelNavegadores -Text " " -X 120 -Y $cy_ar[1] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonbrave.Add_Click({$global:app="XP8C9QZMS2PC1T"; ipackages}) 
-Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Firefox" -X $cx_ar[2] -Y $cy_ar[2] ; $buttonbrave = Add-Button -Control $panelNavegadores -Text " " -X 120 -Y $cy_ar[2] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonfirefox.Add_Click({$global:app="9NZVDKPMR9RD"; ipackages}) 
-Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Opera" -X $cx_ar[3] -Y $cy_ar[3] ; $buttonopera = Add-Button -Control $panelNavegadores -Text " " -X 120 -Y $cy_ar[3] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonfirefox.Add_Click({$global:app="Opera.Opera"; ipackages}) 
-Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Opera GX" -X $cx_ar[4] -Y $cy_ar[4]  ; $buttonoperagx = Add-Button -Control $panelNavegadores -Text " " -X 120 -Y $cy_ar[4] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonfirefox.Add_Click({$global:app="Opera.OperaGX"; ipackages}) 
-
+Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Chrome" -X $cx_ar[0] -Y $cy_ar[0] ; $buttonchrome = Add-Button -Control $panelNavegadores -Text " " -X ($cx_ar[0] - 35) -Y $cy_ar[0] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonchrome.Add_Click({$global:app="Google.Chrome"; ipackages}) 
+Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Brave" -X $cx_ar[1] -Y $cy_ar[1] ; $buttonbrave = Add-Button -Control $panelNavegadores -Text " " -X ($cx_ar[1] - 35) -Y $cy_ar[1] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonbrave.Add_Click({$global:app="XP8C9QZMS2PC1T"; ipackages}) 
+Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Firefox" -X $cx_ar[2] -Y $cy_ar[2] ; $buttonbrave = Add-Button -Control $panelNavegadores -Text " " -X ($cx_ar[2] - 35) -Y $cy_ar[2] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonfirefox.Add_Click({$global:app="9NZVDKPMR9RD"; ipackages}) 
+Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Opera" -X $cx_ar[3] -Y $cy_ar[3] ; $buttonopera = Add-Button -Control $panelNavegadores -Text " " -X ($cx_ar[3] - 35) -Y $cy_ar[3] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonfirefox.Add_Click({$global:app="Opera.Opera"; ipackages}) 
+Add-Label-Clickeable -MiControl $panelNavegadores -Texto "Opera GX" -X $cx_ar[4] -Y $cy_ar[4]  ; $buttonoperagx = Add-Button -Control $panelNavegadores -Text " " -X ($cx_ar[4] - 35) -Y $cy_ar[4] -Width 30 -Height 20 -ForeColor '#FFFFFF';$buttonfirefox.Add_Click({$global:app="Opera.OperaGX"; ipackages}) 
 
 #Elementos de paneles para los drivers
 $labelGPU = Add-Label -control $panelGpuCpuDrivers -Text "Driver de CPU/GPU" -X 0 -Y 5 -Width $panel_width -Height 25 -ForeColor '#8E44AD' -Size 15
@@ -102,9 +101,6 @@ Add-Label-Clickeable -MiControl $panelPlaceholder6 -Texto "Visual Estudio 2022 C
 Add-Label-Clickeable -MiControl $panelPlaceholder6 -Texto "Jetbrains Toolbox" -X $cx_ar[5] -Y $cy_ar[5]
 
 
-
-$button2 = Add-Button -Control $tab2Install -Text "Button 2" -X 100 -Y 500 -Width 200 -Height 100 -ForeColor '#FFFFFF'
-$tweaksTitle = Add-Label -control $tabPage1 -Text "DEBUG TEST" -X 5 -Y 100 -Width 457 -Height 142
 
 
 ajustarPosicionPaneles -Paneles $panelNavegadores, $panelGpuCpuDrivers, $panelCompresor, $panelPlaceholder2, $panelPlaceholder3, $panelPlaceholder4 , $panelplaceholder5 , $panelplaceholder6
