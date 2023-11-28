299cb6776cf73d8c7758ae4fcfb74b4873dedd9c
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-27 23:07:37 -0300

    autohide taskbar completed

--- src/tweaks/autoHideTaskbar.psm1
+++ src/tweaks/autoHideTaskbar.psm1
@@ -1,25 +1,18 @@
 function autoHideTaskbar {
+    $p = 'HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
+    $v = (Get-ItemProperty -Path $p).Settings
 
+    Write-Host "Valor Original: $($v[8])"
 
-$p = 'HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3'
-[ref]$v = (Get-ItemProperty -Path $p).Settings
+    if ($v[8] -eq 123) {
+        $v[8] = 122
+    } elseif ($v[8] -eq 122) {
+        $v[8] = 123
+    }
 
-write-host "DEV QUE APRETASTE"
-write-host "DEV $($v.Value)"
+    Set-ItemProperty -Path $p -Name Settings -Value $v
+    Stop-Process -Force -Name explorer
 
-
-if ($v.Value[8] -eq 123) {
-    $v.Value[8]= 122
-    Set-ItemProperty -Path $p -Name Settings -Value $v.Value
-    Stop-Process -Force -ProcessName explorer
+    Write-Host "Valor actualizado: $($v[8])"
 }
 
-if ($v.Value[8] -eq 122) {
-    $v.Value[8]= 123
-        Set-ItemProperty -Path $p -Name Settings -Value $v.Value
-        Stop-Process -Force -ProcessName explorer
-}
-
-
-
-}
