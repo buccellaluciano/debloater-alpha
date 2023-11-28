424b4413790e825684bd0399a80781910f22c355
Luciano <mistercreepypastasymas5@gmail.com>
2023-11-28 01:04:30 -0300

    a

--- src/utils/installpackages.psm1
+++ src/utils/installpackages.psm1
@@ -6,9 +6,9 @@ function INSTALAR-PACK {
     $job = Start-Job -ScriptBlock {
         param($Pack)
         $install = "winget install $Pack --accept-source-agreements --accept-package-agreements"
-        Write-Host("Installing $Pack")
+        Write-Host("Instalando $Pack")
         Invoke-Expression -Command $install
-        $Result = [System.Windows.MessageBox]::Show("Package $Pack installed successfully.", "Installed", "Ok")
+        $Result = [System.Windows.MessageBox]::Show("Package $Pack Instalado correctamente.", "Instalado", "Ok")
     }   -ArgumentList $Pack
 
     # Espera a que el trabajo en segundo plano termine (sin bloquear la interfaz de usuario)
