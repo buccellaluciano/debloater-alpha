fc5c47961666fbe00ea4f76595e45b36ff09b040
CocolocoDev <cocoloco2005go@gmail.com>
2023-11-28 01:01:47 -0300

    start-job

--- src/utils/installpackages.psm1
+++ src/utils/installpackages.psm1
@@ -2,14 +2,15 @@ function INSTALAR-PACK {
     param (
         [string]$Pack
     )
-        
 
-    $install ="winget install $Pack --accept-source-agreements --accept-package-agreements"
-    Write-Host("$Pack")
-    Invoke-Expression -Command $install
-    $Result=  Add-Type -AssemblyName PresentationCore,PresentationFramework
-    $Result = [System.Windows.MessageBox]::Show("Paquete $Pack correctamente instalado.","Instalado","Ok")
-    
-    
+    $job = Start-Job -ScriptBlock {
+        param($Pack)
+        $install = "winget install $Pack --accept-source-agreements --accept-package-agreements"
+        Write-Host("Installing $Pack")
+        Invoke-Expression -Command $install
+        $Result = [System.Windows.MessageBox]::Show("Package $Pack installed successfully.", "Installed", "Ok")
+    } -ArgumentList $Pack
+
+    # Espera a que el trabajo en segundo plano termine (sin bloquear la interfaz de usuario)
 
 }
